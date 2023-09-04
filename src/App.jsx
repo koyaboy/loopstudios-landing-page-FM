f67c8cae5f2bcdccbab2f4ff08f271deb582df29
Koya <44787464+koyaboy@users.noreply.github.com>
2023-09-04 14:28:20 +0100

    Mobile-Menu-Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./containers/MainContent"
 
 function App() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <Header toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+      {!toggleMenu && (
+        <>
+          <MainContent />
+          <Footer />
+        </>
+      )
+      }
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,41 @@ import logo from "../assets/images/logo.svg"
 import { iconClose, iconHamburger } from '../assets/images/images'
 
 
-const Header = () => {
+const Header = ({ toggleMenu, setToggleMenu }) => {
     return (
         <>
-            <div className='header-mobile px-6 py-8'>
-                <div className='flex justify-between items-center'>
-                    <img src={logo} alt="logo" />
-                    <img src={iconHamburger} alt="icon-hamburger" />
-                </div>
+            {toggleMenu ?
+                (
+                    <div className='bg-black w-screen h-screen px-5'>
+                        <div className='flex justify-between items-center'>
+                            <img src={logo} alt="logo" />
+                            <img src={iconClose} alt="close-icon" onClick={() => setToggleMenu(false)} />
+                        </div>
+                        <nav className='text-white mt-[35%] font-josefin'>
+                            <ul className='flex flex-col gap-4'>
+                                <li><a href="#">ABOUT</a></li>
+                                <li><a href="#">CAREERS</a></li>
+                                <li><a href="#">EVENTS</a></li>
+                                <li><a href="#">PRODUCTS</a></li>
+                                <li><a href="#">SUPPORT</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                )
+                :
+                (
+                    <div className='header-mobile px-6 py-8'>
+                        <div className='flex justify-between items-center'>
+                            <img src={logo} alt="logo" />
+                            <img src={iconHamburger} alt="icon-hamburger" onClick={() => setToggleMenu(true)} />
+                        </div>
 
-                <div className='border border-white pl-4 py-4 relative top-44'>
-                    <div className='heading uppercase text-white text-4xl w-4/5'> Immersive experiences that deliver</div>
-                </div>
-            </div>
+                        <div className='border border-white pl-4 py-4 relative top-44'>
+                            <div className='heading uppercase text-white text-4xl w-4/5'> Immersive experiences that deliver</div>
+                        </div>
+                    </div>
+                )
+            }
         </>
 
     )
